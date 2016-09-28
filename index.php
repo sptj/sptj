54fe856f07af1b907144e78f0cdaeb8c17c8c351
sptj1993 <1144518482@qq.com>
2016-09-28 22:38:11 +0800

    date

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php
-header("Content-Type:text/html; charset=utf-8");
-include('WeChatCallBackAPI.php');
-include('MysqlDataBase.php');
-$wechatObj = new wechatCallbackapiTest();
-$databasemanager=new MysqlManager();
+// header("Content-Type:text/html; charset=utf-8");
+// include('WeChatCallBackAPI.php');
+// include('MysqlDataBase.php');
+// $wechatObj = new wechatCallbackapiTest();
+// $databasemanager=new MysqlManager();
 //$wechatObj->valid();
 //$wechatObj->MsgAnalyse();
 //$Keyword=$wechatObj->GetKeyWord();
 echo "哈哈";
-if($Keyword=='开灯')
-{
-	//$databasemanager->UpdateSwitchState(1);
-	$wechatObj->responseMsg("灯马上就开");
-}
-else if($Keyword=='关灯')
-{
+// if($Keyword=='开灯')
+// {
+	// //$databasemanager->UpdateSwitchState(1);
+	// $wechatObj->responseMsg("灯马上就开");
+// }
+// else if($Keyword=='关灯')
+// {
 
-	//$databasemanager->UpdateSwitchState(0);	
-	$wechatObj->responseMsg("灯马上就关");
-}
-else
-{
-	$wechatObj->responseMsg('对不起，仅能识别“开灯”，“关灯”这两个关键词~');
-}
+	// //$databasemanager->UpdateSwitchState(0);	
+	// $wechatObj->responseMsg("灯马上就关");
+// }
+// else
+// {
+	// $wechatObj->responseMsg('对不起，仅能识别“开灯”，“关灯”这两个关键词~');
+// }
 
 //$databasemanager->CloseConnect();
 
