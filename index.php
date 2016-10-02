039a33d86f161e29dd27825c4a2e7a06f5d14625
sptj1993 <1144518482@qq.com>
2016-10-02 11:48:30 +0800

    today

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header("Content-Type:text/html; charset=utf-8");
+//header("Content-Type:text/html; charset=utf-8");
 include('WeChatCallBackAPI.php');
 //include('MysqlDataBase.php');
 $wechatObj = new wechatCallbackapiTest();
