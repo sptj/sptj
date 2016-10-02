01e25a9036d0346dca62573a2493a930c02b6aef
sptj1993 <1144518482@qq.com>
2016-10-02 12:27:18 +0800

    today

--- index.php
+++ index.php
@@ -1,83 +1,28 @@
 <?php
-/**
-  * wechat php test
-  */
-
-//define your token
-define("TOKEN", "weixin");
+header("Content-Type:text/html; charset=utf-8");
+include('WeChatCallBackAPI.php');
+//include('MysqlDataBase.php');
 $wechatObj = new wechatCallbackapiTest();
+//$databasemanager=new MysqlManager();
 $wechatObj->valid();
-
-class wechatCallbackapiTest
+$wechatObj->MsgAnalyse();
+$Keyword=$wechatObj->GetKeyWord();
+if($Keyword=='开灯')
 {
-	public function valid()
-    {
-        $echoStr = $_GET["echostr"];
 
-        //valid signature , option
-        if($this->checkSignature()){
-			ob_clean();
-        	echo $echoStr;
-			echo "123";
-        	exit;
-        }
-    }
+	//$databasemanager->UpdateSwitchState(1);
+	$wechatObj->responseMsg("灯已开");
+}
+else if('关灯')
+{
 
-    public function responseMsg()
-    {
-		//get post data, May be due to the different environments
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
-      	//extract post data
-		if (!empty($postStr)){
-                
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-                $textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
-							</xml>";             
-				if(!empty( $keyword ))
-                {
-              		$msgType = "text";
-                	$contentStr = "Welcome to wechat world!";
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
-                }else{
-                	echo "Input something...";
-                }
-
-        }else {
-        	echo "";
-        	exit;
-        }
-    }
-		
-	private function checkSignature()
-	{
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];	
-        		
-		$token = TOKEN;
-		$tmpArr = array($token, $timestamp, $nonce);
-		sort($tmpArr);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
-		
-		if( $tmpStr == $signature ){
-			return true;
-		}else{
-			return false;
-		}
-	}
+	//$databasemanager->UpdateSwitchState(0);	
+	$wechatObj->responseMsg("灯未开");
 }
 
+//$databasemanager->CloseConnect();
+
 ?>
+
+
+
